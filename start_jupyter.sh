d5fc7cfd16005272564be49d20fc1911852155d3
Douglas R Miles <logicmoo@gmail.com>
2023-08-29 04:47:07 -0700

    3-VSpaceTest.metta

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -10,15 +10,15 @@
 
 
 #pip install jupyterlab-server
-pip install jupyter
-pip install jupyter_contrib_nbextensions
-pip install jupyter_nbextensions_configurator
-pip install jupyterthemes
-pip install jupyterlab
-pip install jupyter-lsp
-pip install jupyter_lsp
-pip install jupyterlab-lsp
+#pip install jupyter
+#pip install jupyter_contrib_nbextensions
+#pip install jupyter_nbextensions_configurator
+#pip install jupyterthemes
+#pip install jupyterlab
+#pip install jupyter-lsp
+#pip install jupyter_lsp
+#pip install jupyterlab-lsp
 
-pip install -U "jupyter-server<2.0.0"
+#pip install -U "jupyter-server<2.0.0"
 
 jupyter notebook --allow-root --no-browser --port=17888 --ip 0.0.0.0 --password das
--- test_in_metta.sh
+++ test_in_metta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export RUST_BACKTRACE=1
-
+export RUST_BACKTRACE=full
+export PYTHONPATH=./metta_vspace
 #python3 metta_vspace/metta_repl.py
 
-pip install .
-python3 metta_vspace/flyspace.py
-
+#pip install .
+#python3 metta_vspace/flyspace.py
+metta 
