4f249751fe4c5c30ed1d805ea63fc4fe81f1b53b
Douglas R Miles <logicmoo@gmail.com>
2023-08-28 23:47:56 -0700

    metta_learner.py

--- metta_vspace/__init__.py
+++ metta_vspace/__init__.py
@@ -7,8 +7,8 @@ from hyperon.ext import register_tokens
 from pyswip import registerForeign, PL_foreign_context, PL_foreign_control, PL_FIRST_CALL, PL_REDO, PL_PRUNED, PL_retry, PL_FA_NONDETERMINISTIC, Variable, Prolog as PySwip, Atom as PySwipAtom
 gswip = PySwip()
 import hyperon as hp
-from . import flyspace
-from .flyspace import FlySpace
+from . import metta_learner
+from .metta_learner import VSpace
 #from . import metta_learner
 
 
