2ad24e857b76399a017090888cf26323b75ef82f
Douglas R Miles <logicmoo@gmail.com>
2023-09-02 02:15:43 -0700

    chmod

--- test_in_metta.sh
+++ test_in_metta.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 export RUST_BACKTRACE=full
-export PYTHONPATH=./metta_vspace
-#python3 metta_vspace/metta_repl.py
+export PYTHONPATH=.
+/metta_vspace
+#python3 metta_vspace/metta_repl.
+py
 
 #pip install .
-#python3 metta_vspace/flyspace.py
-metta 
+
+#python3 metta_vspace/flyspace.
+py
+metta
+
