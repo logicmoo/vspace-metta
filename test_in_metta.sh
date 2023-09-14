bd6731ec0c42af3a4cb876e163546e74d8365000
Douglas R Miles <logicmoo@gmail.com>
2023-09-14 00:15:08 -0700

    examples/NARS_listing.metta

--- test_in_metta.sh
+++ test_in_metta.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 export RUST_BACKTRACE=full
-export PYTHONPATH=.
-/metta_vspace
-#python3 metta_vspace/metta_repl.
-py
-
+export PYTHONPATH=./metta_vspace
+#python3 metta_vspace/metta_repl.py
 #pip install .
-
-#python3 metta_vspace/flyspace.
-py
+#python3 metta_vspace/flyspace.py
 metta
 
